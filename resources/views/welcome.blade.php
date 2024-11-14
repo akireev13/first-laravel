31605d9c1475e4ec53f3d4bdbb4cc4a401ad4062
akireev13 <akireev13@mail.ru>
2024-11-14 19:50:51 +0100

    Comments update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,18 +2,22 @@
 
     @foreach ($articles as $article)
         <div class="mt-4">
-            <h2 class="font-bold text-lg">{{ $article->title }}</h2>
+            <h2 class="font-bold text-xl">{{ $article->title }}</h2>
             <p>{{ $article->content }}</p>
             {{-- format date for published at ->format('M d') --}}
-            <p class="text-sm text-slate-700">{{ $article->author?->name ?? 'Unknown' }} | {{ $article->published_at }}</p>
-            <h3 class="mt-2">Comments</h3>
-            <ul>
-                @foreach ($article->comments as $comment)
-                    <li>{{ $comment->content }}</li>
-                @endforeach
-            </ul>
+            <p class="text-sm text-slate-700">{{ $article->author?->name ?? 'Unknown' }} | {{ $article->published_at }}
+            </p>
+            @if ($article->comments->count() == 0)
+                <p>No comments yet</p>
+            @else
+                <h3 class="mt-2 text-lg font-semibold">Comments</h3>
+                <ul class="list-disc pl-4">
+                    @foreach ($article->comments->take(3) as $comment)
+                        <li>{{ $comment->content }}</li>
+                    @endforeach
+                </ul>
+            @endif
         </div>
-        
     @endforeach
 
 </x-site-layout>
